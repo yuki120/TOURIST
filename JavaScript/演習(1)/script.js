570e69ed26e2d4b41b9b22c254acd37154ef69c7
yuki120 <xvfqh120@gmail.com>
2018-04-13 10:31:00 +0900

    20180413

---
+++ JavaScript/演習(1)/script.js
@@ -0,0 +1,96 @@
+document.write("問1<br>");
+for(a = 1; a <= 5; a++){
+    document.write("★");
+}
+
+
+
+document.write("<br><br>");
+document.write("問2<br>");
+for(a = 1; a <= 2; a++){
+    for(b = 1; b <= 3; b++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+    
+
+
+document.write("<br><br>");
+document.write("問3<br>");
+for(a = 1; a <= 2; a++){
+    for(b = 1; b <= 5; b++){
+        document.write("☆");
+    }
+    document.write("<br>");
+}
+
+
+
+document.write("<br><br>");
+document.write("問4<br>");
+for(a = 1; a <= 4; a++){
+    for(b = 1; b <= 5; b++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+
+
+
+document.write("<br><br>");
+document.write("問5<br>");
+for(a = 1; a <= 4; a++){
+    for(b = 1; b <= 3; b++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+
+
+
+
+document.write("<br><br>");
+document.write("問6<br>");
+for(a = 1; a <= 3; a++){
+    for(b = 1; b <= 3; b++){
+        if(b % 2 == 0){
+            document.write("☆");
+        }else{
+            document.write("★");
+        }
+    }
+    document.write("<br>");
+}
+
+
+
+document.write("<br><br>");
+document.write("問7<br>");
+for(a = 1; a <= 5; a++){
+    for(b = 1; b <= 5; b++){
+        if(b % 2 == 0){
+            document.write("☆");
+        }else{
+            document.write("★");
+        }
+    }
+    document.write("<br>");
+}
+
+
+document.write("<br><br>");
+document.write("問8<br>");
+for(a = 0; a < 5; a++){
+    for(b = 0; b < a+1; b++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+
+
+
+
+
+
+
